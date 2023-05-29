22c85dab20996b58b7e83c7ee996c4b36da823fe
jenzie68 <jacecabalo@gmail.com>
2023-05-29 16:46:00 +0400

    add displayboard function, add style for box, add div container

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const GameBoard = (player, player2) => {
-   let gameBoard = () => ['','','','','','','','',''];
+   let gameBoard = ['x','o','x','x','','','','',''];
+
+   let displayBoard = () => {
+    gameBoard.forEach((e) => {
+      const container = document.querySelector('.container');
+      const div = document.createElement('div');
+      div.textContent = e;
+      div.classList.add('box');
+      container.appendChild(div);
+    });
+   };
+
+   let win = 0;
+
+   const getWin = () => win;
 
    let switchPlayers = () => {
     player = 'x';
@@ -13,9 +27,16 @@ const GameBoard = (player, player2) => {
    }
 
    let winCheck = () => {
-     
-   }
+    const players = player || player2
 
-   return {gameBoard, switchPlayers, player, player2}     
+    if (gameBoard === [players,players,players]) {
+    }
+   };
+
+   return {gameBoard, displayBoard, switchPlayers, player, player2, getWin}     
 };
+
+const jenny = GameBoard('jenny','AI')
+
+jenny.displayBoard()
 
